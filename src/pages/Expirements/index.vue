c2247161029dbbd906a5daf9777e20cad3998d1f
VKovalev <VKovalev@synergy.ru>
2023-11-01 17:12:37 +0300

    -=-

--- src/pages/Expirements/index.vue
+++ src/pages/Expirements/index.vue
@@ -1,5 +1,32 @@
 <template lang="pug">
-div {{ c1 }} {{ c2 }}
+table(width="100%" style="width: 100%; height: 100%; background-color: white;")
+  tr
+    td(height="100%")
+      table(style="width: 100%; max-width: 600px; min-width: 400px; margin-left: auto; margin-right: auto")
+        tr
+          table(style="background-color:#f5f5f5; width:100%; height: 80px;")
+            tr
+              td
+        tr
+          td
+            p Здравствуйте!
+            p Мы уже создали для Вас Личный кабинет, где Вы сможете посмотреть ознакомительный курс об Университете.
+            p Чтобы завершить регистрацию Личного кабинета, пройдите по ссылке, актуализируйте свои личные данные и задайте пароль.
+            p Нажмите для продолженения регистрации.
+            p Внимание! Ссылка действительна 12 часов
+            p После того, как вы завершите регистрацию, Личный кабинет будет доступен Вам по ссылке.
+            p Благодарим вас за доверие к Университету «Синергия» и желаем успехов в обучении!
+        tr
+          td
+        tr
+          td
+            table(style="background-color:#252525; width:100%; height: 148px;")
+              tr
+                td
+                  table(style="margin-left: auto; margin-right: auto; font-size: 13px; color: white;")
+                    tr
+                      td
+                        p © 1988 — 2023, Университет «Синергия». Все права защищены.
 </template>
   
 <script setup lang="ts">
@@ -17,4 +44,16 @@ const c2 = computed(() => {
 })
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+
+table {
+  border: 1px black solid;
+  border-spacing: 0;
+  tr {
+    box-sizing: border-box;
+    td {
+      padding: 0;
+    }
+  }
+}
+</style>
