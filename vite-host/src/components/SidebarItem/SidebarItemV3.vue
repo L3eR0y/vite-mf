f99c016391a21aed1ef690dbdb5c52d03c28d8ef
Vitaliy Kovalev <vkovalev@synergy.ru>
2024-02-04 18:54:16 +0300

    on Menu and submenu items click

--- vite-host/src/components/SidebarItem/SidebarItemV3.vue
+++ vite-host/src/components/SidebarItem/SidebarItemV3.vue
@@ -3,7 +3,7 @@
   slot
     Accordion(:expanded="!minimized" :item="item")
       template(#head)
-        NavItem(:item="item" :minimized="minimized")
+        NavItem(:item="item" :minimized="minimized" @click="onMenuItemClick")
       template(#body)
         .submenu-container(v-if="item.submenu")
           NavItem.submenu-item(              
@@ -11,11 +11,15 @@
             :key="(submenuItem.route || '') + index"
             :minimized="minimized"
             submenu-item
-            :item="submenuItem")
+            :item="submenuItem"
+            @click="onSubmenuItemClick(submenuItem)")
 </template>
 
 <script setup lang="ts">
 import { toRefs } from 'vue'
+
+import type { SidebarMenuItem } from '@/types/sidebar.type'
+
 import Accordion from '@components/Accordion/Accordion.vue'
 import NavItem from '@components/NavItem/NavItem.vue'
 
@@ -31,6 +35,14 @@ const props = defineProps({
 })
 
 const { item } = toRefs(props)
+
+function onMenuItemClick() {
+  console.log('Menuitem: ', item.value)
+}
+
+function onSubmenuItemClick(submenuitem: SidebarMenuItem) {
+  console.log('Submenuitem: ', submenuitem)
+}
 </script>
 
 <style scoped lang="scss">
