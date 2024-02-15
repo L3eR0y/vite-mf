68b5a902e41fe393b73310e057939030517d579e
Panteleeva Nastya <anastasiia.samartceva@gmail.com>
2024-02-15 18:09:29 +0300

    delete profile

--- vite-remote-profile/src/auth/LocalStorageMap.ts
+++
@@ -1,5 +0,0 @@
-export type LocalStorageMapping = {
-  "access_token": string,
-  "refresh_token": string,
-  "exp": string
-}
--- vite-remote-profile/src/components/Profile/ProfileTabs/Tabs/PersonalDoc/PersonalDoc.vue
+++
@@ -1,9 +0,0 @@
-<template lang="pug">
-.presonal-doc-tab PERSONAL DOC
-</template>
-
-<script setup lang="ts">
-</script>
-
-<style scoped lang="scss">
-</style>
