9b55a93dfea4590b2caefd70a5fe08bd7c454fd9
Vitaliy Kovalev <vkovalev@synergy.ru>
2024-02-23 21:57:06 +0300

    Navigation bar

--- vite-host/src/components/NavigationBar/NavigationBar.vue
+++ vite-host/src/components/NavigationBar/NavigationBar.vue
@@ -1,12 +1,22 @@
 <template lang="pug">
 .navigation-bar-wrapper
-  .content
+  .left-side-container
     Search
-  .avatar
+  .right-side-container
+    .right-side-container__avatar 
+      AccountCircleOutline.icon(fill-color="#353741" :size="46")
+    .right-side-container__notification
+      BellBadgeOutline.icon(fill-color="#98989A")
+    .right-side-container__messages
+      MessageTextOutline.icon(fill-color="#98989A")
 </template>
 
 <script setup lang="ts">
 import Search from '@components/Search/Search.vue'
+import BellBadgeOutline from 'vue-material-design-icons/BellBadgeOutline.vue'
+import MessageTextOutline from 'vue-material-design-icons/MessageTextOutline.vue'
+import AccountCircleOutline from 'vue-material-design-icons/AccountCircleOutline.vue'
+
 </script>
 
 <style scoped lang="scss">
@@ -18,17 +28,27 @@ import Search from '@components/Search/Search.vue'
   justify-content: space-between;
 }
 
-.content {
+.left-side-container {
   width: 100%;
   flex-grow: 1;
 }
 
-.avatar {
-  width: 44px;
-  height: 44px;
-  border-radius: 50%;
-  border: 1px $br-main solid;
-  flex-grow: 0;
-  flex-shrink: 0;
+.right-side-container {
+  display: flex;
+  align-items: center;
+  flex-direction: row-reverse;
+  gap: 24px;
+
+  &__notification, &__messages, &__avatar {
+    width: 44px;
+    height: 44px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+}
+
+.icon {
+  cursor: pointer;
 }
 </style>
--- vite-host/src/components/Search/Search.vue
+++ vite-host/src/components/Search/Search.vue
@@ -3,6 +3,7 @@
   .search-wrapper__icon
     MagnifyIcon(fill-color="#98989A")
   .search-wrapper__input
+    input.search-input
 </template>
 
 <script setup lang="ts">
@@ -18,5 +19,20 @@ import MagnifyIcon from 'vue-material-design-icons/Magnify.vue'
   display: flex;
   align-items: center;
   padding: 10px 12px;
+
+  &__input {
+    width: 100%;
+  }
+}
+
+.search-input {
+  width: 100%;
+  background-color: transparent;
+  border: none;
+  color: $text-color-main;
+
+  &:focus {
+    outline: none;
+  }
 }
 </style>
